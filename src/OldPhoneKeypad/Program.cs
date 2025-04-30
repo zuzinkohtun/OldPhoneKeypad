17035534b2c7afb6f7c39e6a040a1c8798399e69
Zu Zin Ko Htun <zuzinkohtun@gmail.com>
2025-04-30 17:29:38 +0700

    Map the key for phonekeypad

--- src/OldPhoneKeypad/Program.cs
+++ src/OldPhoneKeypad/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OldPhoneKeyPad;
+
+string? nextTry = "";
+
+Console.WriteLine("***** Welcome from Phone KeyPad *****");
+do{
+    Console.WriteLine("Please type your input : ");
+    string? input = Console.ReadLine();
+
+    string output = OldPhonePadExtension.OldPhonePad(input);
+    Console.WriteLine("Output: " + output);
+
+    Console.WriteLine("Type (Y or Yes) for next try!");
+    nextTry = Console.ReadLine();
+
+}while(nextTry?.ToLower() == "y" || nextTry?.ToLower() == "yes");
+
