de5beb9d562d21b55c10d038bdc50d9b582adf8c
Zu Zin Ko Htun <zuzinkohtun@gmail.com>
2025-04-30 17:57:57 +0700

    Code refactoring version 1

--- src/OldPhoneKeypad/Program.cs
+++ src/OldPhoneKeypad/Program.cs
@@ -3,7 +3,17 @@
 string? nextTry = "";
 
 Console.WriteLine("***** Welcome from Phone KeyPad *****");
-do{
+
+Console.WriteLine("33# =>" + " Output: " + OldPhonePadExtension.OldPhonePad("33#"));
+Console.WriteLine("227*# =>" + " Output: " + OldPhonePadExtension.OldPhonePad("227*#"));
+Console.WriteLine("4433555 555666# =>" + " Output: " + OldPhonePadExtension.OldPhonePad("4433555 555666#"));
+Console.WriteLine("8 88777444666*664# =>" + " Output: " + OldPhonePadExtension.OldPhonePad("8 88777444666*664#"));
+
+Console.WriteLine("Do you want to try by yourself? If yes, please type 'Y' or 'Yes'");
+nextTry = Console.ReadLine();
+
+while(nextTry?.ToLower() == "y" || nextTry?.ToLower() == "yes")
+{
     Console.WriteLine("Please type your input : ");
     string? input = Console.ReadLine();
 
@@ -12,6 +22,4 @@ do{
 
     Console.WriteLine("Type (Y or Yes) for next try!");
     nextTry = Console.ReadLine();
-
-}while(nextTry?.ToLower() == "y" || nextTry?.ToLower() == "yes");
-
+}
