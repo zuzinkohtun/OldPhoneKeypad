bc55d73f44b6451719e4dbbcf766d88bb7b861e4
Zu Zin Ko Htun <zuzinkohtun@gmail.com>
2025-05-08 18:38:52 +0700

    Modified version using Json file for key store

--- src/OldPhoneKeypad/OldPhonePadExtension.cs
+++ src/OldPhoneKeypad/OldPhonePadExtension.cs
@@ -1,74 +1,105 @@
-namespace OldPhoneKeyPad;
-public class OldPhonePadExtension{
-    private static readonly Dictionary<char, string> keypad = new Dictionary<char, string>
-    {
-        { '1', "&'(" },
-        { '2', "ABC" },
-        { '3', "DEF" },
-        { '4', "GHI" },
-        { '5', "JKL" },
-        { '6', "MNO" },
-        { '7', "PQRS" },
-        { '8', "TUV" },
-        { '9', "WXYZ" },
-        { '0', " " }
-    };
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text.Json;
 
-    public static string OldPhonePad(string? input){
-        if(string.IsNullOrEmpty(input)) return "";
-        
+namespace OldPhoneKeyPad;
+public class OldPhonePadExtension
+{
+    private static readonly List<KeyStore>? keys = new();
+    private static readonly Dictionary<char, int> keyRange = new();
+
+    static OldPhonePadExtension()
+    {
+        string filePath = "KeyStore.json";
+
+        if (File.Exists(filePath))
+        {
+            string jsonData = File.ReadAllText(filePath);
+            keys = JsonSerializer.Deserialize<List<KeyStore>>(jsonData);
+
+            //Calculate key ranges to avoid the over range
+            foreach (var key in keys)
+            {
+                char keyCharacter = (string.IsNullOrEmpty(key.value)) ? '\0' : char.ToUpper(key.key[0]);
+                if (keyRange.ContainsKey(keyCharacter)) keyRange[keyCharacter]++;
+                else keyRange.Add(keyCharacter, 1);
+            }
+        }
+        else
+        {
+            Console.WriteLine("JSON file not found.");
+        }
+    }
+
+    public static string OldPhonePad(string? input)
+    {
+        // Early return for null data
+        if (string.IsNullOrEmpty(input)) return ""; 
+
+        // Check to end with '#'
         if (input[^1] != '#')
             return "Input value must end with '#'. Please try again. Thanks";
 
-        string currentValue = string.Empty;
-        char lastChar = '\0';   
-        string result = string.Empty;
+        // Transform to listed string value
+        var listedValue = SplitDataAsList(input);
 
-        for(int i=0;i<input.Length;i++){
-            char currentData = input[i];
-            char nextChar = '\0';
-
-            switch(currentData){
-                case '#': // For end case
-                    return result;
-                case ' ': // For encoding the character
-                    result += MapCharacter(currentValue);
-                    // Reset the current values
-                    currentValue = string.Empty;
-                    lastChar = '\0';
-                    break;
-                case '*': // For delete case
-                    result = (string.IsNullOrEmpty(result)? result : result.Substring(0,result.Length-1));
-                    break;
-                default: // Actual processing
-                    if(currentData == lastChar || currentValue == "") {
-                        currentValue += currentData;
-                        nextChar = input[i + 1];
-                        if(currentData != nextChar) {
-                            result += MapCharacter(currentValue);
-                            currentValue = ""; 
-                        }
-                    }
-                    else{
-                        result += MapCharacter(currentValue);
-                        currentValue = currentData.ToString(); 
-                    }
-                    lastChar = currentData;
-                    break;
-            }
-        }  
-        return result;  
+        // Decode to string from listed string as digits and characters
+        return DecodeString(listedValue);
     }
 
-    private static string MapCharacter(string currentValue){
-        if(string.IsNullOrWhiteSpace(currentValue)) return currentValue;
+    private static string DecodeString(List<string> requestValue)
+    {
+        string result = string.Empty;
+        foreach (var each in requestValue)
+        {
+            if (each == "#") return result;
+            else if (each == "*")
+                result = (string.IsNullOrEmpty(result)) ? result : result.Substring(0, result.Length - 1);
 
-        char keyChar = currentValue[0];
-        if(!keypad.ContainsKey(keyChar)) return "";
+            else
+                result += keys?.Where(k => k.key == each).Select(k => k.value).FirstOrDefault();
+        }
+        return result;
+    }
 
-        string currentLetters = keypad[keyChar];
+    private static List<string> SplitDataAsList(string input)
+    {
+        var result = new List<string>();
+        var splitValue = input.Split(' ');
 
-        int index = (currentValue.Length - 1) % currentLetters.Length; // To avoid over value
-        return currentLetters[index].ToString();
+        foreach (var eachValue in splitValue)
+        {
+            var multiTimeData = String.Empty;
+            for (int i = 0; i < eachValue.Length; i++)
+            {
+                // If have same value in next index
+                if (i < eachValue.Length - 1 && eachValue[i] == eachValue[i + 1] && eachValue[i] != '*')
+                {
+                    multiTimeData += eachValue[i];
+                    continue;
+                }
+                // For one time character or digit
+                else if (string.IsNullOrEmpty(multiTimeData)) 
+                    result.Add(eachValue[i].ToString());   
+                // For multi-time input characters or digits          
+                else
+                {
+                    multiTimeData += eachValue[i];
+                    // Checking the range and then calculate if over
+                    if (keyRange.ContainsKey(eachValue[i]))
+                    {
+                        int acceptableRange = keyRange[eachValue[i]];
+                        // Recalculate the value if over the range
+                        if ((multiTimeData.Length > acceptableRange))
+                        {
+                            multiTimeData = multiTimeData.Substring(0, multiTimeData.Length % acceptableRange);
+                        }
+                    }
+                    result.Add(multiTimeData);
+                    multiTimeData = String.Empty; // Reset data as null
+                }
+            }
+        }
+        return result;
     }
 }
